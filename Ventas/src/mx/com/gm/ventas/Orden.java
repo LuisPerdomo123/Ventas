4b9d6ad288d6076d3c7b2ac7b5645a18a6b6cc63
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-02-05 18:04:17 -0500

    Diseño de clases parte VI

--- Ventas/src/mx/com/gm/ventas/Orden.java
+++ Ventas/src/mx/com/gm/ventas/Orden.java
@@ -9,52 +9,52 @@ package mx.com.gm.ventas;
  * @author lepo9
  */
 public class Orden {
-    private int idOreden;
+
+    private int idOrden;
     private Producto productos[];
     private static int contadorOrdenes;
-    private int contadorProducto;
+    private int contadorProductos;
     private static final int MAX_PRODUCTOS = 10;
-    
-    public Orden(){
-    
-        this.idOreden = ++Orden.contadorOrdenes;
+
+    public Orden() {
+
+        this.idOrden = ++Orden.contadorOrdenes;
         this.productos = new Producto[Orden.MAX_PRODUCTOS];
     }
-    
-    public void agregarProducto(Producto producto){
-    
-        if(this.contadorProducto < Orden.MAX_PRODUCTOS){
-        
-            this.productos[this.contadorOrdenes++] = producto;
-        }
-        else{
-        
+
+    public void agregarProducto(Producto producto) {
+
+        if (this.contadorProductos < Orden.MAX_PRODUCTOS) {
+
+            this.productos[this.contadorProductos++] = producto;
+        } else {
+
             System.out.println("Se ha superado el maximo de productos: " + Orden.MAX_PRODUCTOS);
         }
     }
-    
-    public double calcularTotal(){
-    
+
+    public double calcularTotal() {
+
         double total = 0;
-        for (int i = 0; i < this.contadorProducto; i++) {
+        for (int i = 0; i < this.contadorProductos; i++) {
 //            Producto producto = this.productos[i];
 //            total += producto.getPrecio();//total = total + producto.getPrecio()
             total += this.productos[i].getPrecio();
         }
-        
+
         return total;
     }
-    
-    public void mostrarOrden(){
-    
-        System.out.println("Id Orden: " + this.idOreden);
+
+    public void mostrarOrden() {
+
+        System.out.println("Id Orden: " + this.idOrden);
         double totalOrden = this.calcularTotal();
-        System.out.println("Total de la orden: $"+totalOrden);
+        System.out.println("Total de la orden: $" + totalOrden);
         System.out.println("Productos de las Orden: ");
-        for (int i = 0; i < this.contadorProducto; i++) {
+        for (int i = 0; i < this.contadorProductos; i++) {
             System.out.println(this.productos[i]);
         }
-        
+
     }
-    
+
 }
