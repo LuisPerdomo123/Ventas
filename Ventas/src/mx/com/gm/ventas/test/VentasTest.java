d3f1c24ce5dd756277c8dc4995232a2b7f98754a
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-02-05 18:03:08 -0500

    Diseño de clases parte VI

---
+++ Ventas/src/mx/com/gm/ventas/test/VentasTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.gm.ventas.test;
+
+import mx.com.gm.ventas.*;
+
+/**
+ *
+ * @author lepo9
+ */
+public class VentasTest {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Camisa",50.00);
+        Producto producto2 = new Producto("Pantalon", 100.00);
+        
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
+        
+        
+    }
+}
